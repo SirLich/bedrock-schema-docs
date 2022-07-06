33ad7a781046b2f09cda28c9fc2bc987f2ec771d
SirLich <lrkoehler77@gmail.com>
2022-07-06 23:57:06 +0200

    Add searchbar and search count

--- index.js
+++ index.js
@@ -1,47 +1,49 @@
-const blocks = document.querySelectorAll('.open-block');
-blocks.forEach(block => {
-	block.addEventListener('click', function handleClick(event) {
-		block.nextSibling.classList.toggle('invisible');
-		block.nextSibling.nextSibling.classList.toggle('hidden');
-	});
+const blocks = document.querySelectorAll(".open-block");
+blocks.forEach((block) => {
+  block.addEventListener("click", function handleClick(event) {
+    block.nextSibling.classList.toggle("invisible");
+    block.nextSibling.nextSibling.classList.toggle("hidden");
+  });
 });
 
-const extenders = document.querySelectorAll('.extender');
-extenders.forEach(block => {
-	block.addEventListener('click', function handleClick(event) {
-		block.classList.toggle('invisible');
-		block.nextSibling.classList.toggle('hidden');
-	});
+const extenders = document.querySelectorAll(".extender");
+extenders.forEach((block) => {
+  block.addEventListener("click", function handleClick(event) {
+    block.classList.toggle("invisible");
+    block.nextSibling.classList.toggle("hidden");
+  });
 });
 
+function collapseAll() {
+  const blocks = document.querySelectorAll(".open-block");
+  blocks.forEach((block) => {
+	block.nextSibling.classList.toggle("invisible");
+	block.nextSibling.nextSibling.classList.toggle("hidden");
+  });
+}
 
 function filter() {
+  var searchbar = document.getElementById("searchbar");
 
-	var searchbar = document.getElementById('searchbar');
+  var filter = searchbar.value.toUpperCase();
 
-	var filter = searchbar.value.toUpperCase();
-	
-	var elements = document.querySelectorAll('.component');
-	
-	elements.forEach(element => {
-		let searchTerm = element.className.toUpperCase();
-		console.log(searchTerm)
-		if (searchTerm.includes(filter)) {
-			element.style.display = 'block';
-		}
-		else 
-		{
-			element.style.display = 'none';
-		}
-	});
+  var elements = document.querySelectorAll(".component");
 
-	for (i = 0; i < li.length; i++) {
-	  a = li[i].getElementsByTagName("a")[0];
-	  txtValue = a.textContent || a.innerText;
-	  if (txtValue.toUpperCase().indexOf(filter) > -1) {
-		li[i].style.display = "";
-	  } else {
-		li[i].style.display = "none";
-	  }
-	}
-  }
+  var filterCount = 0;
+
+  elements.forEach((element) => {
+    let searchTerm = element.className.toUpperCase();
+    console.log(searchTerm);
+    if (searchTerm.includes(filter)) {
+      filterCount += 1;
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
+    }
+  });
+
+  var counter = document.getElementById("counter");
+  counter.innerHTML = filterCount;
+}
+
+filter();
